de56443bec282e19163aa9e5b17677ef5cd726a8
aravindd7 <ak154761@gmail.com>
2022-05-30 11:57:32 +0530

    razorpay api added

---
+++ routes/razorpay.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const Doctor = require("../models/Doctor");
+
+const router = express.Router();
+require('dotenv').config();
+const razorpay = new Razorpay({
+  key_id: process.env.APIKEY,
+  key_secret: process.env.SECRETKEY,
+});
+
+router.get('/', (req, res) => {
+    res.send('Welcome to Razorpay Api');
+});
+
+
+router.post("/createOrder", async (req, res) => {
+  const order = await razorpay.orders.create({
+    amount: req.body.amount,
+    currency: req.body.currency,
+  });
+  res.send(order);
+});
+
+
+module.exports = router;
