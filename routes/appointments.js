1803450434e45d0530f3bd689138e1d150524638
aravindd7 <ak154761@gmail.com>
2022-06-03 14:17:23 +0530

    msg

--- routes/appointments.js
+++ routes/appointments.js
@@ -12,63 +12,66 @@ router.post("/", (req, res) => {
     acceptStatus: req.body.acceptStatus,
   });
 
-  appointment.save()
-  .then(result => {
+  appointment
+    .save()
+    .then((result) => {
       res.send({
-          message: "Appointment created successfully",
-          data: result
-      })
-  })
-  .catch(err => console.log(err))
+        message: "Appointment created successfully",
+        data: result,
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
 // /api/appointments
-router.get('/', (req, res) => {
-    Appointment.find()
-        .then(appointments => {
-            res.send(appointments)
-        })
-        .catch(err => console.log(err))
+router.get("/", (req, res) => {
+  Appointment.find()
+    .then((appointments) => {
+      res.send(appointments);
+    })
+    .catch((err) => console.log(err));
 });
 
 // /api/appointment/accept/id
-router.post('/accept/:id', (req, res) => {
-    const appointmentId = req.params.id;
+router.put("/accept/:id", (req, res) => {
+  const appointmentId = req.params.id;
 
-    Appointment.findById(appointmentId)
-        .then(appointment => {
-            appointment.acceptStatus = true;
-            appointment.save(function(err) {
-                if(!err) {
-                    console.log("Appointment Accept Status updated");
-                }
-                else {
-                    console.log("Error: appointment Accept update status fail");
-                }
-            });
-            res.send(appointment);
-        })
-        .catch(err => console.log(err))
+  Appointment.findById(appointmentId)
+    .then((appointment) => {
+      if (!appointment) {
+        console.log("No appointment found");
+      } else {
+        appointment.acceptStatus = true;
+        appointment.save(function (err) {
+          if (!err) {
+            console.log("Appointment Accept Status updated");
+          } else {
+            console.log("Error: appointment Accept update status fail");
+          }
+        });
+        res.send(appointment);
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
 // /api/appointment/start/id
-router.post('/start/:id', (req, res) => {
-    const appointmentId = req.params.id;
+router.post("/start/:id", (req, res) => {
+  const appointmentId = req.params.id;
 
-    Appointment.findById(appointmentId)
-        .then(appointment => {
-            appointment.acceptStatus = true;
-            appointment.save(function(err) {
-                if(!err) {
-                    console.log("Appointment Start Status updated");
-                }
-                else {
-                    console.log("Error: appointment start update status fail");
-                }
-            });
-            res.send(appointment);
-        })
-        .catch(err => console.log(err))
+  Appointment.findById(appointmentId)
+    .then((appointment) => {
+      appointment.acceptStatus = true;
+      appointment.save(function (err) {
+        if (!err) {
+          console.log("Appointment Start Status updated");
+        } else {
+          console.log("Error: appointment start update status fail");
+        }
+      });
+      res.send(appointment);
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
